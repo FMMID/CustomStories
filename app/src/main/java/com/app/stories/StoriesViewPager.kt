bfd53100327e1f490f864e58d0b6d6024aa1ab40
Mihail <mikhail.gerasimets@joy-dev.com>
2022-09-09 09:50:43 +0300

    xxx: исправлена ошибка плеера при частом пролистывании

--- app/src/main/java/com/app/stories/StoriesViewPager.kt
+++ app/src/main/java/com/app/stories/StoriesViewPager.kt
@@ -11,11 +11,15 @@ import android.widget.VideoView
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 
-class StoriesViewPagerAdapter(val stories: List<Story>, private val segmentProgressBarCallback: (StoriesEvent) -> Unit) :
-    RecyclerView.Adapter<StoriesViewPagerViewHolder>() {
+class StoriesViewPagerAdapter(
+    val stories: List<Story>,
+    private val segmentProgressBarCallback: (StoriesEvent) -> Unit
+) : RecyclerView.Adapter<StoriesViewPagerViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoriesViewPagerViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.stories_view_pager_item, parent, false)
+        val itemView = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.stories_view_pager_item, parent, false)
         return StoriesViewPagerViewHolder(itemView, segmentProgressBarCallback)
     }
 
@@ -31,6 +35,7 @@ class StoriesViewPagerViewHolder(
     itemView: View,
     private val segmentProgressBarCallback: (StoriesEvent) -> Unit
 ) : RecyclerView.ViewHolder(itemView) {
+
     private val videoPlaceholder: VideoView = itemView.findViewById(R.id.stories_view_pager_item_video_view)
     private val imagePlaceholder: ImageView = itemView.findViewById(R.id.stories_view_pager_item_image_view)
     private var mediaPlayer: MediaPlayer? = null
@@ -47,9 +52,7 @@ class StoriesViewPagerViewHolder(
                 MotionEvent.ACTION_DOWN -> {
                     timeDown = System.currentTimeMillis()
                     segmentProgressBarCallback.invoke(StoriesEvent.OnPauseEvent)
-                    if (videoPlaceholder.isVisible) {
-                        mediaPlayer?.pause()
-                    }
+                    playVideo(play = false)
                 }
                 MotionEvent.ACTION_UP -> {
                     timeUp = System.currentTimeMillis()
@@ -63,14 +66,12 @@ class StoriesViewPagerViewHolder(
                             }
                             else -> {
                                 segmentProgressBarCallback.invoke(StoriesEvent.OnStartEvent)
-                                mediaPlayer?.start()
+                                playVideo(play = true)
                             }
                         }
                     } else {
                         segmentProgressBarCallback.invoke(StoriesEvent.OnStartEvent)
-                        if (videoPlaceholder.isVisible) {
-                            mediaPlayer?.start()
-                        }
+                        playVideo(play = true)
                     }
                 }
             }
@@ -89,18 +90,27 @@ class StoriesViewPagerViewHolder(
     }
 
     fun bind(story: Story) {
-        when (story) {
+        imagePlaceholder.isVisible = when (story) {
             is Story.VideoStory -> {
-                imagePlaceholder.isVisible = false
-                videoPlaceholder.isVisible = true
                 videoPlaceholder.setVideoURI(story.uriVideo)
                 videoPlaceholder.start()
+                false
             }
             is Story.ImageStory -> {
-                imagePlaceholder.isVisible = true
-                videoPlaceholder.isVisible = false
                 imagePlaceholder.setImageURI(story.uriImage)
+                true
             }
+        }
+        videoPlaceholder.isVisible = !imagePlaceholder.isVisible
+    }
+
+    /**
+     * @param play - true, то попробовать воспроизвести видео, иначе остановить
+     */
+    private fun playVideo(play: Boolean) {
+        when {
+            play && videoPlaceholder.isVisible && mediaPlayer?.isPlaying == false -> mediaPlayer?.start()
+            !play && videoPlaceholder.isVisible && mediaPlayer?.isPlaying == true -> mediaPlayer?.stop()
         }
     }
 }
